aaaed2d61999682fbe5ed5938b9171f9a0a8626d
huangbq4gh <huangbq.01@gmail.com>
2014-05-17 14:40:00 -0400

    Initial Commit

---
+++ Photo Kangaroo/bingqingAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  bingqingAppDelegate.h
+//  Photo Kangaroo
+//
+//  Created by Bingqing Huang on 5/17/14.
+//  Copyright (c) 2014 Bingqing Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface bingqingAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Photo Kangaroo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Photo Kangaroo
+//
+//  Created by Bingqing Huang on 5/17/14.
+//  Copyright (c) 2014 Bingqing Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "bingqingAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([bingqingAppDelegate class]));
+    }
+}
